f46e5701d3d32508c20d0b6726d41efd7c27d6fb
Adminis <anoopdevopseng@gmail.com>
2025-07-26 19:58:08 +0530

    Update debug point

--- resources/nat_router.py
+++ resources/nat_router.py
@@ -10,50 +10,36 @@ def create_nats(scope, project_id, nat_list,vpc_modules):
         env = nat_cfg["environment"]
         vpc_name = nat_cfg["vpc_id"]
 
+        depends_on = []
+
         if vpc_name in vpc_modules:
-            # VPC is defined internally
-            vpc_module = vpc_modules[vpc_name]
-            vpc_self_link = vpc_module.output["vpc"]["self_link"]
-
-            nat_module = TerraformModule(
-                scope,
-                f"{vpc_name}-nat-{env}",
-                source="git::https://github.com/anoopdevopseng/terraform-google-cloudnat?ref=v0.1.0"
-            )
-
-            nat_module.add_override("project_id", project_id)
-            nat_module.add_override("environment", env)
-            nat_module.add_override("region", nat_cfg["region"])
-            nat_module.add_override("vpc_id", vpc_self_link)
-            nat_module.add_override("num_nat_ips", nat_cfg.get("num_nat_ips", 1))
-            nat_module.add_override("subnet_ids_to_nat", nat_cfg["subnet_ids_to_nat"])
-
-            nat_module.add_override("depends_on", [vpc_module.fqn])
-
-            nat_modules[env] = nat_module
-            all_modules[env] = nat_module
+            depends_on = [vpc_modules[vpc_name].fqn]
         
-        else:
         # Lookup VPC self_link by name
-            vpc_lookup = DataGoogleComputeNetwork(
-                scope,
-                f"{vpc_name}-{env}",
-                name=vpc_name,
-                project=project_id
-            )
-            nat_module = TerraformModule(
-                scope,
-                f"{vpc_name}-nat-{env}",
-                source="git::https://github.com/anoopdevopseng/terraform-google-cloudnat?ref=v0.1.0"
-            )
-            nat_module.add_override("project_id", project_id)
-            nat_module.add_override("environment", env)
-            nat_module.add_override("region", nat_cfg["region"])
-            nat_module.add_override("vpc_id", vpc_lookup.self_link)
-            nat_module.add_override("num_nat_ips", nat_cfg.get("num_nat_ips", 1))
-            nat_module.add_override("subnet_ids_to_nat", nat_cfg["subnet_ids_to_nat"])
+        vpc_lookup = DataGoogleComputeNetwork(
+            scope,
+            f"data-{vpc_name}-{env}",
+            name=vpc_name,
+            project=project_id
+        )
 
-            nat_modules[env] = nat_module
-            all_modules[env] = nat_module
+        if depends_on:
+            vpc_lookup.add_override("depends_on", depends_on)
+
+        nat_module = TerraformModule(
+            scope,
+            f"{vpc_name}-nat-{env}",
+            source="git::https://github.com/anoopdevopseng/terraform-google-cloudnat?ref=v0.1.0"
+        )
+
+        nat_module.add_override("project_id", project_id)
+        nat_module.add_override("environment", env)
+        nat_module.add_override("region", nat_cfg["region"])
+        nat_module.add_override("vpc_id", vpc_lookup.self_link)
+        nat_module.add_override("num_nat_ips", nat_cfg.get("num_nat_ips", 1))
+        nat_module.add_override("subnet_ids_to_nat", nat_cfg["subnet_ids_to_nat"])
+
+        nat_modules[env] = nat_module
+        all_modules[env] = nat_module
 
     return nat_modules, all_modules
