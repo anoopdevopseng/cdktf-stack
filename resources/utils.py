ac9ed0e146ac2fe461c34806c279dd28e64ebaba
Adminis <anoopdevopseng@gmail.com>
2025-07-27 05:56:37 +0530

    remove resources

--- resources/utils.py
+++
@@ -1,8 +0,0 @@
-import os
-import yaml
-
-def load_config(path):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Config file not found: {path}")
-    with open(path) as f:
-        return yaml.safe_load(f)
