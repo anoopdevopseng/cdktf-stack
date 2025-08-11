67a8b108b4d5772734a97ecc4139a61fbad62b15
Adminis <anoopdevopseng@gmail.com>
2025-08-12 03:13:21 +0530

    Update cdktf.json and move the module versions in it

--- resources/gke.py
+++ resources/gke.py
@@ -1,4 +1,5 @@
 from cdktf import TerraformModule
+from imports.gke import Gke as gke
 
 def create_gke_clusters(scope, project_id, gke_list, all_modules):
     gke_outputs = {}
@@ -8,15 +9,17 @@ def create_gke_clusters(scope, project_id, gke_list, all_modules):
         vpc_name = gke_cfg["vpc_name"]
         subnet_name = gke_cfg["subnetwork"]
 
-        gke_module = TerraformModule(
+        gke_module = gke(
             scope,
             f"gke-{gke_name}",
-            source="git::https://github.com/anoopdevopseng/terraform-google-gke?ref=v0.1.0",
+            project_id=project_id,
+            environment=gke_cfg["environment"],
+            name=gke_name,
+            node_pools=gke_cfg["node_pools"],
+            services_secondary_range_name=gke_cfg["services_secondary_range_name"],
+            cluster_secondary_range_name=gke_cfg["cluster_secondary_range_name"]
         )
 
-        gke_module.add_override("project_id", project_id)
-        gke_module.add_override("name", gke_name)
-        gke_module.add_override("environment", gke_cfg["environment"])
         gke_module.add_override("location", gke_cfg["location"])
         gke_module.add_override("vpc_name", vpc_name)
         gke_module.add_override("subnetwork", subnet_name)
@@ -24,9 +27,6 @@ def create_gke_clusters(scope, project_id, gke_list, all_modules):
         gke_module.add_override("enable_private_endpoint", gke_cfg.get("enable_private_endpoint", False))
         gke_module.add_override("master_global_access_config", gke_cfg.get("master_global_access_config", True))
         gke_module.add_override("gcp_public_cidrs_access_enabled", gke_cfg.get("gcp_public_cidrs_access_enabled", True))
-        gke_module.add_override("cluster_secondary_range_name", gke_cfg["cluster_secondary_range_name"])
-        gke_module.add_override("services_secondary_range_name", gke_cfg["services_secondary_range_name"])
-        gke_module.add_override("node_pools", gke_cfg["node_pools"])
         gke_module.add_override("argocd", gke_cfg.get("argocd", False))
         if "dns_allow_external_traffic" in gke_cfg:
             gke_module.add_override("dns_allow_external_traffic", gke_cfg["dns_allow_external_traffic"])
